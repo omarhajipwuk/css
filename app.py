ae5f93097cb74c45427b4aeaac7e3a47dae40fae
omarhajipwuk <o.haji@powertica.com>
2025-09-30 08:36:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -425,8 +425,10 @@ else:
 # =========================
 # Footer
 # =========================
+st.write(df.columns)
 st.write("---")
 st.caption(" note to self -- fix table switching errors.")
+
 
 
 
